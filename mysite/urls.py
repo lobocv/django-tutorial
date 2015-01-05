9bb5083a81bb3f0618d6e5b1029e9c968aeebb4b
lobocv <elephant123>
2015-01-04 19:33:44 -0500

    Finished Django tutorial. Added a home page with CSS styling

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from home import views
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^$', 'home.views.index'),
+	url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
