d5c0fc0cd57990137d5521179ddc851b0a4fdb77
lobocv <elephant123>
2015-01-15 17:43:39 -0500

    Added a template to contain the top navigation bar

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from mysite import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'mysite.views.index'),	
-	url(r'^projects/', 'mysite.views.projects'),	
-	url(r'^about/', 'mysite.views.about'),	
-	url(r'^contact/', 'mysite.views.contact'),	
+    url(r'^$', 'mysite.views.index'),
+    url(r'^projects/', 'mysite.views.projects'),
+    url(r'^about/', 'mysite.views.about'),
+    url(r'^contact/', 'mysite.views.contact'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
+
 def index(request):
-    return render_to_response('mysite/index.html', context_instance=RequestContext(request))
-	
+    return render_to_response('mysite/index.html',
+                              {'page_title': 'The Wolf Den'},
+                              context_instance=RequestContext(request))
+
+
 def projects(request):
-    return render_to_response('mysite/projects.html', context_instance=RequestContext(request))
-	
+    return render_to_response('mysite/projects.html',
+                              {'page_title': 'Projects'},
+                              context_instance=RequestContext(request))
+
+
 def about(request):
-    return render_to_response('mysite/about.html', context_instance=RequestContext(request))
+    return render_to_response('mysite/about.html',
+                              {'page_title': 'About'},
+                              context_instance=RequestContext(request))
+
 
 def contact(request):
-    return render_to_response('mysite/contact.html', context_instance=RequestContext(request))
+    return render_to_response('mysite/contact.html',
+                              {'page_title': 'Contact'},
+                              context_instance=RequestContext(request))
 		
